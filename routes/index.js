9f62d76cb2b5630d1438655aee49705ec76a362c
e22i <e22i@fresnou.org>
2025-01-15 08:43:47 -0800

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let toDoItems = [
+    {'id': 1, 'description': 'wash dishes', 'completed': false},
+    {'id': 2, 'description': 'fold laundry', 'completed': false},
+    {'id': 3, 'description': 'do homework', 'completed': false},
+  ];
+  res.render('index', {toDoItems});
+});
+
+module.exports = router;
